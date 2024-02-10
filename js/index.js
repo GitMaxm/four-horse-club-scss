68b31cca9a4216988e88e4d1eb8a227e6c6d66fb
Ivan <ivanenkomax@rambler.ru>
2024-02-10 12:53:25 +0300

    add

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-const swiper = new Swiper('.swiper', {
-    loop: loop,
+const swiper = new Swiper('.chessplayers__swiper', {
+    loop: true,
     slidesPerView: 3, // Отображаем по три слайда за раз
     spaceBetween: 30, // Расстояние между слайдами
     navigation: {
@@ -23,13 +23,43 @@ const swiper = new Swiper('.swiper', {
             slidesPerView: 1,
             spaceBetween: 10
         },
-        768: {
+        867: {
             slidesPerView: 2,
             spaceBetween: 20
         },
-        1024: {
+        1197: {
             slidesPerView: 3,
             spaceBetween: 30
+        },
+    }
+});
+
+
+const swiper2 = new Swiper('.steps__swiper', {
+    // Optional parameters
+    loop: false,
+    spaceBetween: 30,
+    slidesPerView: 1,
+
+    // If we need pagination
+    pagination: {
+        el: '.steps__swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.steps__swiper-button-next',
+        prevEl: '.steps__swiper-button-prev',
+    },
+
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10
+        },
+        867: {
+            slidesPerView: 2,
+            spaceBetween: 20
         },
     }
 });
